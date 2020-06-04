3ae706030d11044de7a5cfffde07d64f487a70e6
Aian Soktoev <soktoevayan@gmail.com>
2020-06-04 22:29:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,9 @@ def load_library(path)
   emoticons = YAML.load_file(path)
   newHash = {}
   emoticons.each do |key, value| 
-    
+    newHash[key] = key
+    newHash[key]["english"] = value[0]
+    newHash[key]["japanese"] = value[1]
 end
 
 
